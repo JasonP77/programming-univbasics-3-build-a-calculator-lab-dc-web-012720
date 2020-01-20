8602b7723a82fdad3ed049ae08358ef2835b5c64
Jin Hyuk Park <llvslr77@gmail.com>
2020-01-20 20:24:49 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,21 +4,26 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  sub = num2 - num1
+  return sub
 end
 
 def division(num1, num2)
-
+  div = num2/num1
+  return div
 end
 
 def multiplication(num1, num2)
-
+  multi = num2*num1
+  return multi
 end
 
 def modulo(num1, num2)
-
+  mod = num2%num1
+  return mod
 end
 
 def square_root(num)
-
+  sq = Math.sqrt(num)
+  return sq
 end
